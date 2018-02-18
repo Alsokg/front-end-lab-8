0116476529e562d176d37b371930da65de557609
Kolya Zubekhin <aloskg@gmail.com>
2018-02-18 21:42:43 +0000

    check arguments list

--- homework_08/js/getMin.js
+++ homework_08/js/getMin.js
@@ -1,5 +1,10 @@
 function getMin() {
+  if (arguments.length === 0) {
+    return undefined;
+  }
+  
   var min = arguments[0];
+  
 	for (var i = 1; i < arguments.length; i++) {
 		min = Math.min(min, arguments[i]);
 	}
